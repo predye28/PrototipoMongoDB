69aec56ffb48947580ee69b2ce13de3aff5e4cef
Omar Madrigal <80352580+predye28@users.noreply.github.com>
2023-10-31 21:26:24 -0600

    Final

--- PrototipoMongoDb/app.js
+++ PrototipoMongoDb/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({ extended: true })); // Middleware para manejar datos POST
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/prototipo', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,13 +19,21 @@ mongoose.connection.once('open', () => {
   console.log('Conexión a MongoDB establecida con éxito');
 });
 
-const usuarioSchema = new mongoose.Schema({
+const EmpleadoSchema = new mongoose.Schema({
     usuario: String,
     contrasena: String,
     nombre: String
   }, { collection: 'empleado' }); 
   
-const Empleado = mongoose.model('Empleado', usuarioSchema);
+const Empleado = mongoose.model('Empleado', EmpleadoSchema);
+
+const productoSchema = new mongoose.Schema({
+  numeroProducto: String,
+  nombre: String,
+  cantidad: String
+}, { collection: 'producto' }); 
+
+const Producto = mongoose.model('Producto', productoSchema);
 
 app.post('/procesar_registro', async (req, res) => {
     try {
@@ -63,7 +71,44 @@ app.post('/procesar_login', async (req, res) => {
       console.error('Error al procesar el inicio de sesión:', error);
       res.status(500).send('Error al procesar el inicio de sesión');
     }
-  });
+});
+
+app.post('/procesar_registroDocumento', async (req, res) => {
+  try {
+    //mongodb
+    const nuevoProducto = new Producto({
+      numeroProducto: req.body.numeroProducto,
+      nombre: req.body.nombre,
+      cantidad: req.body.cantidad,
+
+    });
+    await nuevoProducto.save();
+
+    
+    const mensaje = "El Producto se ha ingresado correctamente.";
+
+      res.send(
+        `<script>
+          alert("${mensaje}");
+          window.location.href = '/lobby.html';
+        </script>`
+      );
+  } catch (error) {
+    console.error('Error al registrar el usuario:', error);
+    res.status(500).send('Error al registrar el usuario');
+  }
+});
+
+app.get('/obtenerProductos', async (req, res) => {
+  try {
+    const productos = await Producto.find({}, 'nombre'); 
+    res.json(productos);
+
+  } catch (error) {
+    console.error('Error al obtener la lista de usuarios:', error);
+    res.status(500).json({ error: 'Error al obtener la lista de usuarios' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor Express en funcionamiento en el puerto ${port}`);
